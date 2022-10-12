8fbe1fee4fc689126a5aff0b2293f6293526c9ad
Delfika <kramorov4345@gmail.com>
2022-10-12 15:01:42 +0300

    Встреча любимчика, с командой, которая пренебригает написанием способа имени

---
+++ Lesson_5_privetstvie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "виталик")
+{
+    Console.WriteLine("Ура, это же Виталик");
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.WriteLine("Привет,");
+}
