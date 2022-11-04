a9091d95a70dcb3f29dac58597a6e1b01b40dfee
Delfika <kramorov4345@gmail.com>
2022-11-04 18:41:45 +0300

    Коммит домашки, задание 19

---
+++ Homework_3.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+Console.WriteLine("Введите число");
+int Number =int.Parse(Console.ReadLine()!);
+int Number1 = Number % 10;
+int Number2 = (Number/10)%10; /// 123 4
+int Number3 = ((Number/10)/10)%10;
+int Number4 = (((Number/10)/10)/10)%10;
+int Number5 = ((((Number/10)/10)/10)/10)%10;
+if (Number5 == Number1 && Number2 == Number4) Console.WriteLine("YES");
+else Console.WriteLine("NO");
