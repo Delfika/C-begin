4db93ce6c5ff930e84c208bc1bc7f97cbb40173a
Delfika <kramorov4345@gmail.com>
2022-11-09 19:49:48 +0300

    Коммит 25 задания

---
+++ Homework_4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Double Koren(double a,double b)
+{ 
+    double c = Math.Pow(a,b);
+    return c;
+}
+Console.WriteLine("Введите число А:");
+double a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B:");
+double b = int.Parse(Console.ReadLine()!);
+double result = Koren(a,b);
+Console.WriteLine("Результат возведения А в степень В = "+result);
