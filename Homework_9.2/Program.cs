0b23d9d066da6aaa058548e840e267c9a6c05bf6
Delfika <kramorov4345@gmail.com>
2022-12-04 15:46:41 +0300

    Коммит 66 задания

---
+++ Homework_9.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int Summ(int m,int n)
+{
+    if (n==m)
+    return n;
+    return n + Summ(m,n - 1);
+}
+Console.WriteLine("Введите значение M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение N");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма элементов = {Summ(m,n)}");
+
+
